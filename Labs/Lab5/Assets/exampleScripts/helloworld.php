b18fa4c5924ec691a74e2075e1708e5a74933af4
J Corbett <jcorb8800@gmail.com>
2021-02-26 15:08:36 +0000

    lab5 setup

---
+++ Labs/Lab5/Assets/exampleScripts/helloworld.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=UTF-8"/>
+<title>Hello World</title>
+</head>
+  <p>HTML says Hello World</p>
+    <script type="type/javascript">
+      document.write('<p>javascript says hello world</p>');
+    </script>
+    <?php
+      echo "<p>PHP says Hello World</p>";
+    ?>
+
+<body>  
+</body>
+</html>
