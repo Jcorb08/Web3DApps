43a9cee5180435c8a981a1e0fe7fe4aad1ec4f02
J Corbett <jcorb8800@gmail.com>
2021-04-20 17:11:19 +0100

    file struc

---
+++ assignment/application/mvc.php
@@ -0,0 +1,15 @@
+<?php
+// The require or include statement takes all the text/code/markup that exists 
+// in the specified file and copies it into the file that uses the include (or require) statement.
+require 'view/load.php';
+require 'model/model.php';
+require 'controller/controller.php';
+// The URI which was given in order to access this page; for instance, '/index.html'. 
+$pageURI =$_SERVER['REQUEST_URI'];
+// Returns part of the string that is the page we are accessing
+$pageURI =substr($pageURI,strrpos($pageURI,'index.php')+10);
+	if (!$pageURI)
+		new Controller('home');
+	else
+		new Controller($pageURI);
+?>
