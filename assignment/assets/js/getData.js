6dcd30058e1b779fa45e12cddf6977b50ae734c5
J Corbett <jcorb8800@gmail.com>
2021-04-25 21:15:41 +0100

    data test

--- assignment/assets/js/getData.js
+++ assignment/assets/js/getData.js
@@ -5,24 +5,24 @@ $(document).ready(function(){
     // can see the object returned
     console.log(jsonObj);
     // get home page text
-    $('#title_home').html('<h2>' + jsonObj.pageTextData[0].title + '</h2>');
-    $('#subtitle_home').html('<h3>' + jsonObj.pageTextData[0].subtitle + '</h3>');
-    $('#text_home').html('<p>' + jsonObj.pageTextData[0].text + '</p>');
+    $('#title_home').html('<h2>' + jsonObj.pageTextData[0].Title + '</h2>');
+    $('#subtitle_home').html('<h3>' + jsonObj.pageTextData[0].Subtitle + '</h3>');
+    $('#text_home').html('<p>' + jsonObj.pageTextData[0].Paragraph + '</p>');
 
-    // home left text
-    $('#title_left').html('<h2>' + jsonObj.pageTextData[1].title + '</h2>');
-    $('#subtitle_left').html('<h3>' + jsonObj.pageTextData[1].subtitle + '</h3>');
-    $('#text_left').html('<p>' + jsonObj.pageTextData[1].text + '</p>');
+    // cocacola text
+    $('#title_cocacola').html('<h2>' + jsonObj.pageTextData[1].Title + '</h2>');
+    $('#subtitle_cocacola').html('<h3>' + jsonObj.pageTextData[1].Subtitle + '</h3>');
+    $('#text_cocacola').html('<p>' + jsonObj.pageTextData[1].Paragraph + '</p>');
 
-    // home centre text
-    $('#title_centre').html('<h2>' + jsonObj.pageTextData[2].title + '</h2>');
-    $('#subtitle_centre').html('<h3>' + jsonObj.pageTextData[2].subtitle + '</h3>');
-    $('#text_centre').html('<p>' + jsonObj.pageTextData[2].text + '</p>');
+    // sprite text
+    $('#title_sprite').html('<h2>' + jsonObj.pageTextData[2].Title + '</h2>');
+    $('#subtitle_sprite').html('<h3>' + jsonObj.pageTextData[2].Subtitle + '</h3>');
+    $('#text_sprite').html('<p>' + jsonObj.pageTextData[2].Paragraph + '</p>');
 
-    // home right text
-    $('#title_right').html('<h2>' + jsonObj.pageTextData[3].title + '</h2>');
-    $('#subtitle_right').html('<h3>' + jsonObj.pageTextData[3].subtitle + '</h3>');
-    $('#text_right').html('<p>' + jsonObj.pageTextData[3].text + '</p>');
+    // drpepper text
+    $('#title_drpepper').html('<h2>' + jsonObj.pageTextData[3].Title + '</h2>');
+    $('#subtitle_drpepper').html('<h3>' + jsonObj.pageTextData[3].Subtitle + '</h3>');
+    $('#text_drpepper').html('<p>' + jsonObj.pageTextData[3].Paragraph + '</p>');
     
     /*
     // coke 
